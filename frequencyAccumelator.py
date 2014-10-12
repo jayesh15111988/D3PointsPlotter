b8ccab8e935f3fe7cbce42b6002654a4735b623b
Jayesh Kawli <j.kawli@gmail.com>
2014-10-11 22:08:30 -0400

    Initial commit with all data points plotting enabled

---
+++ frequencyAccumelator.py
@@ -0,0 +1,24 @@
+import csv
+
+frequencyHolder={};
+with open('spamMessagesFrequency.csv', 'Ur') as f:
+    finalList = list(tuple(rec) for rec in csv.reader(f, delimiter=','))
+
+def writeDictionaryInCSVFile(dictionaryToWrite,fileName):
+    with open(fileName, 'w', newline='') as fp:
+        CSVFileWriter = csv.writer(fp, delimiter=',')
+        for word, frequency in dictionaryToWrite.items():
+            CSVFileWriter.writerow([word, frequency]);
+
+for index, individualTuple in enumerate(finalList):
+    currentTuple=individualTuple;
+    tempList=[];
+    if currentTuple[1] in frequencyHolder:
+        tempList=frequencyHolder[currentTuple[1]];
+        tempList.append(currentTuple[0]);
+    else:
+        tempList.append(currentTuple[0]);
+    frequencyHolder[currentTuple[1]]=tempList;
+writeDictionaryInCSVFile(frequencyHolder,"spamMessagesGraphCompliant.csv");
+
+        
